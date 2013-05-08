3b4f5fac1c924dfe4861ca327a9f61ff8cdb2657
Mickael Riga <mig@mypeplum.com>
2013-05-08 09:35:59 +0100

    Refactor with more PP macros

--- bricklane.c
+++ bricklane.c
@@ -9,7 +9,7 @@
 #define WORD_SIZE 32
 #define BUFFER_SIZE 4096
 #define NEXT goto *ip++->p
-#define CELL_SIZE (sizeof(void*))
+#define CELL_SIZE (sizeof(cell_t))
 #define HEADER(N,L,I,H) dp->p = link; link = dp++; \
   dp->p = (word_metadata *)malloc(sizeof(word_metadata)); \
   ((word_metadata*)dp->p)->length = L; \
@@ -17,9 +17,9 @@
   ((word_metadata*)dp->p)->hidden = H; \
   strcpy(((word_metadata*)dp->p)->name, N); \
   dp++
-
-#define VERSION_MAJOR 0 /* not backward compatible */
-#define VERSION_MINOR 0 /* backward compatible */
+#define DICT(V) dp++->p = V
+#define DICT_I(V) dp++->i = V
+#define PRIMITIVE(N,L,I,H,A) HEADER(N,L,I,H); DICT(A)
 
 typedef union {
   intptr_t i;
@@ -57,65 +57,64 @@ int main(int argc, const char *argv[])
   char temp_char, *end;
   div_t divmod_result;
   
-  HEADER("VERSION_MAJOR",13,0,0); dp++->p = &&LIT; dp++->i = VERSION_MAJOR;
-  HEADER("VERSION_MINOR",13,0,0); dp++->p = &&LIT; dp++->i = VERSION_MINOR;
-  HEADER("YES",3,0,0); dp++->p = &&LIT; dp++->i = -1;
-  HEADER("NO",2,0,0); dp++->p = &&LIT; dp++->i = 0;
+  HEADER("YES",3,0,0); DICT(&&LIT); DICT_I(-1);
+  HEADER("NO",2,0,0); DICT(&&LIT); DICT_I(0);
 
-  HEADER("state",5,0,0); dp++->p = &&LIT; dp++->p = &state;
-  HEADER("base",4,0,0); dp++->p = &&LIT; dp++->p = &base;
+  HEADER("state",5,0,0); DICT(&&LIT); DICT(&state);
+  HEADER("base",4,0,0); DICT(&&LIT); DICT(&base);
 
-  HEADER("show-stack",10,0,0); dp++->p = &&SHOW_STACK;
-  HEADER("drop",4,0,0); dp++->p = &&DROP;
-  HEADER("swap",4,0,0); dp++->p = &&SWAP;
-  HEADER("dup",3,0,0); dp++->p = &&DUP;
-  HEADER("over",4,0,0); dp++->p = &&OVER;
-  HEADER("dig",3,0,0); dp++->p = &&DIG;
-  HEADER("bury",4,0,0); dp++->p = &&BURY;
+  PRIMITIVE("show-stack",10,0,0,&&SHOW_STACK);
+  PRIMITIVE("drop",4,0,0,&&DROP);
+  PRIMITIVE("swap",4,0,0,&&SWAP);
+  PRIMITIVE("dup",3,0,0,&&DUP);
+  PRIMITIVE("over",4,0,0,&&OVER);
+  PRIMITIVE("dig",3,0,0,&&DIG);
+  PRIMITIVE("bury",4,0,0,&&BURY);
   /* 2drop 2dup 2swap ?dup */
-  HEADER("1+",2,0,0); dp++->p = &&INCREMENT;
-  HEADER("1-",2,0,0); dp++->p = &&DECREMENT;
-  HEADER("+",1,0,0); dp++->p = &&PLUS;
-  HEADER("-",1,0,0); dp++->p = &&MINUS;
-  HEADER("*",1,0,0); dp++->p = &&MULTIPLY;
-  HEADER("/mod",4,0,0); dp++->p = &&DIVMOD;
+  PRIMITIVE("1+",2,0,0,&&INCREMENT);
+  PRIMITIVE("1-",2,0,0,&&DECREMENT);
+  PRIMITIVE("+",1,0,0,&&PLUS);
+  PRIMITIVE("-",1,0,0,&&MINUS);
+  PRIMITIVE("*",1,0,0,&&MULTIPLY);
+  PRIMITIVE("/mod",4,0,0,&&DIVMOD);
   /* u/mod */
-  HEADER("nest",4,0,0); dp++->p = &&NEST;
-  HEADER("unnest",6,0,0); dp++->p = &&UNNEST;
-  HEADER("lit:",4,0,0); dp++->p = &&LIT;
-  HEADER("@",1,0,0); dp++->p = &&FETCH;
-  HEADER("!",1,0,0); dp++->p = &&STORE;
+  PRIMITIVE("nest",4,0,0,&&NEST);
+  PRIMITIVE("unnest",6,0,0,&&UNNEST);
+  PRIMITIVE("lit:",4,0,0,&&LIT);
+  PRIMITIVE("@",1,0,0,&&FETCH);
+  PRIMITIVE("!",1,0,0,&&STORE);
   /* +! -! c@ c! c@c! cmove */
-  HEADER("key",3,0,0); dp++->p = &&KEY;
-  HEADER("emit",4,0,0); dp++->p = &&EMIT;
-  HEADER("word:",5,0,0); dp++->p = &&WORD;
-  HEADER("number",6,0,0); dp++->p = &&NUMBER;
-  HEADER("find",4,0,0); dp++->p = &&FIND;
-  HEADER("code-field",10,0,0); dp++->p = &&CODE_FIELD;
-  HEADER("create",6,0,0); dp++->p = &&CREATE;
-  HEADER(",",1,0,0); dp++->p = &&COMMA;
-  HEADER("[",1,1,0); dp++->p = &&STOP_COMPILER;
-  HEADER("]",1,0,0); dp++->p = &&START_COMPILER;
-  HEADER("jump",4,0,0); dp++->p = &&JUMP;
-  HEADER("bye",3,0,0); dp++->p = &&BYE;
-  HEADER("quit",4,0,0); dp++->p = &&QUIT;
+  PRIMITIVE("key",3,0,0,&&KEY);
+  PRIMITIVE("emit",4,0,0,&&EMIT);
+  PRIMITIVE("word:",5,0,0,&&WORD);
+  PRIMITIVE("number",6,0,0,&&NUMBER);
+  PRIMITIVE("find",4,0,0,&&FIND);
+  PRIMITIVE("code-field",10,0,0,&&CODE_FIELD);
+  PRIMITIVE("create",6,0,0,&&CREATE);
+  PRIMITIVE(",",1,0,0,&&COMMA);
+  PRIMITIVE("[",1,1,0,&&STOP_COMPILER);
+  PRIMITIVE("]",1,0,0,&&START_COMPILER);
+  PRIMITIVE("jump",4,0,0,&&JUMP);
+  PRIMITIVE("bye",3,0,0,&&BYE);
+  PRIMITIVE("quit",4,0,0,&&QUIT);
   
   /* >r r> rsp@ rsp! rdrop */
   /* dsp@ dsp! */ 
 
   HEADER("interpret",9,0,0); 
-  dp++->p = &&NEST;
-  dp++->p = &&WORD; dp++->p = &&SHOW_STACK; dp++->p = &&UNNEST;
-  //dp++->p = &&WORD; dp++->p = &&FIND;
-  //dp++->p = &&CODE_FIELD; dp++->p = &&EXECUTE;
+  DICT(&&NEST);
+  DICT(&&WORD); DICT(&&SHOW_STACK); DICT(&&UNNEST);
+  //DICT(&&WORD); DICT(&&FIND);
+  //DICT(&&CODE_FIELD); DICT(&&EXECUTE);
 
-  HEADER("reset",5,0,0); dp++->p = &&NEST;
-  dp++->p = (dp-8); dp++->p = &&BYE; //dp++->p = &&JUMP; dp++->i = -2;
+  HEADER("reset",5,0,0); DICT(&&NEST);
+  DICT(dp-7);
+  DICT(&&SHOW_STACK); DICT(&&DEBUG); DICT(&&BYE); //DICT(&&JUMP); DICT_I(-2);
 
-  ip = (dp-2);
+  ip = (dp-5);
   NEXT;
 DEBUG: puts("y"); NEXT;
-NEST: rp++->p = ip++; NEXT;
+NEST: rp++->p = ip; NEXT;
 UNNEST: ip = --rp->p; NEXT;
 LIT: *sp++ = *ip++; NEXT;
 FETCH: *(sp-1) = *(cell_t*)(sp-1)->p; NEXT;
